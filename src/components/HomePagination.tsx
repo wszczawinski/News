4d83e43e2b47a430bd278fe6451f9f422c1b220f
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2025-08-22 22:11:11 +0200

    feat(ui improves): pagination, nav

--- src/components/HomePagination.tsx
+++ src/components/HomePagination.tsx
@@ -1,4 +1,4 @@
-import { Link, useRouterState } from "@tanstack/react-router";
+import { Link, useRouterState } from '@tanstack/react-router';
 
 import {
   Pagination,
@@ -8,75 +8,71 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from "./ui/pagination";
+} from './ui/pagination';
 
 export const HomePagination = () => {
-  const { search, pathname } = useRouterState({ select: (s) => s.location });
+  const { search } = useRouterState({ select: s => s.location });
+
   const page = search?.page || 1;
-  const category = search?.category || "all";
+  const category = search?.category || 'all';
 
   return (
     <Pagination>
       <PaginationContent>
-        {page <= 2 ? (
-          <Link
-            disabled={page === 1}
-            to="/news"
-            search={{ page: page - 1, category }}
-          >
+        {page === 1 ? (
+          <Link disabled to='/'>
             <PaginationItem>
-              <PaginationPrevious />
+              <PaginationPrevious className='hover:bg-transparent' />
             </PaginationItem>
           </Link>
         ) : (
-          <Link to="/news" search={{ page: page - 1, category }}>
+          <Link to='/news' search={{ page: page - 1, category }}>
             <PaginationItem>
               <PaginationPrevious />
             </PaginationItem>
           </Link>
         )}
 
-        {pathname === "/" ? (
-          <Link to="/">
+        {page > 2 && (
+          <>
+            <Link to='/news' search={{ page: 1, category }}>
+              <PaginationItem>
+                <PaginationLink> 1 </PaginationLink>
+              </PaginationItem>
+            </Link>
             <PaginationItem>
-              <PaginationLink isActive>{page}</PaginationLink>
+              <PaginationEllipsis />
             </PaginationItem>
-          </Link>
-        ) : (
-          <Link to="/news" search={{ page: page, category }}>
-            <PaginationItem>
-              <PaginationLink isActive>{page}</PaginationLink>
-            </PaginationItem>
-          </Link>
+          </>
         )}
-        <Link
-          disabled={page > 2}
-          to="/news"
-          search={{ page: page + 1, category }}
-        >
+
+        <Link disabled to='/news' search={{ page: page, category }}>
+          <PaginationItem>
+            <PaginationLink isActive className='hover:bg-transparent'>
+              {page}
+            </PaginationLink>
+          </PaginationItem>
+        </Link>
+
+        <Link to='/news' search={{ page: page + 1, category }}>
           <PaginationItem>
             <PaginationLink>{page + 1}</PaginationLink>
           </PaginationItem>
         </Link>
-        <Link
-          disabled={page > 2}
-          to="/news"
-          search={{ page: page + 2, category }}
-        >
+
+        <Link to='/news' search={{ page: page + 2, category }}>
           <PaginationItem>
             <PaginationLink>{page + 2}</PaginationLink>
           </PaginationItem>
         </Link>
+
         <PaginationItem>
           <PaginationEllipsis />
         </PaginationItem>
+
         <PaginationItem>
           <PaginationLink>
-            <Link
-              disabled={page > 2}
-              to="/news"
-              search={{ page: page + 1, category }}
-            >
+            <Link to='/news' search={{ page: page + 1, category }}>
               <PaginationNext />
             </Link>
           </PaginationLink>
